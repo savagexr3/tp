6039c45d54cac5134f0b0e661736dfaaf0178dde
Alastair Tan <atcw@hotmail.sg>
2026-04-08 23:46:34 +0800

    Fix codecov issue

--- src/test/java/seedu/clinkedin/MainAppTest.java
+++ src/test/java/seedu/clinkedin/MainAppTest.java
@@ -1,12 +1,14 @@
 package seedu.clinkedin;
 
-import org.junit.jupiter.api.Test;
-import seedu.clinkedin.commons.core.Version;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.Test;
+
+import seedu.clinkedin.commons.core.Version;
+
 public class MainAppTest {
+
     @Test
     public void version_isCorrect() {
         assertNotNull(MainApp.VERSION);
